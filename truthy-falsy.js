9a282f0460b779d9beb7244a2163925d0b4c903f
Carlos Thompson <carlos.4thons@gmail.com>
2022-01-10 07:43:42 +0000

    truthy and falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,31 @@
+// boolean
+const userLogged = true;
+const bill = false;
+
+// Truthy or Falsy
+
+// 0  = false
+// 1 = true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// null
+// undefined
+
+let myVar;
+let varNull = null;
+
+console.log(myVar);
+console.log(varNull);
+
+
+let number = 3;
+let text = "Thom";
+
+console.log(typeof number);
+console.log(typeof text);
+
+console.log(typeof myVar);
+console.log(typeof varNull);
