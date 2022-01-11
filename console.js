1b4ee0f8ae8b75878183cd178af37b9b9e327a63
Carlos Thompson <ce-thompson@criticalsoftware.com>
2022-01-11 00:56:42 +0000

    javascript and nodejs console, reading errors

---
+++ console.js
@@ -0,0 +1,14 @@
+// Log = registro
+
+const myVar = true;
+
+console.log(245);
+console.log("I'm a text");
+console.log(myVar);
+
+// Tratamento de erro!
+console.error('Deu erro!');
+
+// Other methods:
+console.table = visualizar de forma mais organizada informações tabulares.console
+
---
+++ error.js
@@ -0,0 +1,9 @@
+// const number; 
+// SyntaxError: Missing initializer in const declaration
+
+const number = 1;
+// console.log(number
+// SyntaxError: missing ) after argument list
+
+// console.log(myVar);
+// ReferenceError: myVar is not defined
